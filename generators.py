7e630cd979039d16314f0ab8887d7af2f4806dc2
foxlooo <junwenjiang@hotmail.com>
2020-09-13 13:18:50 -0400

    Added Generator

--- generators.py
+++ generators.py
@@ -16,3 +16,15 @@ newNumsGen = (x*x for x in [1,2,3,4,5])
 print(newNumsNotGenerator)
 print(newNumsGen)
 print(list(newNumsGen))
+
+# Infinite Generaor #
+
+def squareInf(num = 10):
+    i = 0
+    while i != num:
+        yield i * i
+        i += 1
+
+for x in squareInf():
+    print(x)
+
